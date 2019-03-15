dc77ca1e17c8ef3d192507ca6073f0374b3c832f
Maxim Zubkov <Zubkov.MD@phystech.edu>
2019-03-15 16:23:59 +0300

    schema added

---
+++ schema_001.sql
@@ -0,0 +1,43 @@
+CREATE TABLE "webpage" (
+	"id" serial,
+	"url" VARCHAR(255),
+	"model" VARCHAR(255) NOT NULL,
+	"user_id" INTEGER NOT NULL,
+	CONSTRAINT webpage_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "users" (
+	"id" serial,
+	"name" VARCHAR(255),
+	CONSTRAINT users_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "data" (
+	"id" serial NOT NULL,
+	"webpage_id" INTEGER NOT NULL,
+	"x" FLOAT NOT NULL,
+	"y" FLOAT NOT NULL,
+	"w" FLOAT NOT NULL,
+	"h" FLOAT NOT NULL,
+	"isClique" BOOLEAN NOT NULL,
+	"time" TIME NOT NULL,
+	CONSTRAINT data_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "webpage" ADD CONSTRAINT "webpage_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+
+
+ALTER TABLE "data" ADD CONSTRAINT "data_fk0" FOREIGN KEY ("webpage_id") REFERENCES "webpage"("id");
+
