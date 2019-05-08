41b80c7e20a7e2d9adf63fa4c0a616c077d1afdb
turk0v <turkoov@gmail.com>
2019-05-08 19:06:20 +0300

    added popup and button to deactivate extension

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,18 +1,2 @@
-console.log('background script')
-// chrome.tabs.query({},function(tabs) 
-// 	{for (var i = 0;i<tabs.length;i++) 
-// 		{if (tabs[i].active) 
-// 			{
-// 				chrome.tabs.sendMessage(tabs[i].id,'hi');
-// 				console.log(tabs[i].id)
-// 			}
-// 		}
-// 	console.log(tabs)})
-// chrome.browserAction.onClicked.addListener(buttonClicked);
-// chrome.browserAction.onClicked.addListener(buttonClicked);
-// function buttonClicked(tab) {
-// 	setInterval(chrome.tabs.sendMessage(tab.id,'hi'),5000);
-// }
-chrome.alarms.onAlarm.addListener(function( alarm ) {
-  console.log("Got an alarm!", alarm);
-});
+var isExtensionOn = false;
+console.log(isExtensionOn)
